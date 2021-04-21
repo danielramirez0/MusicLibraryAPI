4c3e6dff29be6fbdfa1e25d1b9d24e1b81e79028
Daniel Ramirez <notused@notused.net>
2021-04-21 16:13:47 -0700

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const repoContext = require("./repository/repository-wrapper");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+  console.log("Server started. Listening on port 3000.");
+});
+
+app.get("/api/products", (req, res) => {
+  const products = repoContext.products.findAllProducts();
+  return res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const id = req.params.id;
+  const product = repoContext.products.findProductById(id);
+  return res.send(product);
+});
+
+app.post("/api/products", (req, res) => {
+  const newProduct = req.body;
+  const addedProduct = repoContext.products.createProduct(newProduct);
+  return res.send(addedProduct);
+});
+
+app.put();
+app.delete();
