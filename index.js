d6a6053d70aa8220de3290940db679d1b3873926
Daniel Ramirez <notused@notused.net>
2021-04-26 13:20:53 -0700

    Adds middleware function for validation of submitted data. Adds CORS functionality to permit access to site data and resources

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 //create a Node.js and Express.js backend server for my React Music Library Project.
 const express = require("express");
 const repoContext = require("./repository/repository-wrapper");
+const cors = require("cors");
+const { validateProduct } = require("./middleware//products-validation");
 
 const app = express();
 const listenPort = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// For Global Validation Middleware, use:
+// app.use(validateProduct)
 app.listen(5000, function () {
   console.log(`Server started. Listening on port ${listenPort}.`);
 });
@@ -29,15 +34,16 @@ app.get("/api/songs/:id", (req, res) => {
 });
 
 // Have a POST endpoint, so I can add a new song to the data set.
-app.post("/api/songs", (req, res) => {
+// Have middleware validation for my POST endpoint to ensure data integrity.
+app.post("/api/songs", [validateProduct], (req, res) => {
   const newSong = req.body;
   const addedSong = repoContext.songs.createSong(newSong);
   return res.send(addedSong);
 });
-//TODO:Have middleware validation for my POST endpoint to ensure data integrity.
 
 // Add a PUT endpoint to enable update CRUD functionality.
-app.put("/api/songs/:id", (req, res) => {
+// Have middleware validation for my PUT endpoint to ensure data integrity.
+app.put("/api/songs/:id", [validateProduct], (req, res) => {
   const id = req.params.id;
   const songPropertiesToUpdate = req.body;
   const updateSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
